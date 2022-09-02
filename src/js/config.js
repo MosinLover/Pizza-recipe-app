c5a2cc9f7308bf9c1c0d889244c8df31a738e318
MosinLover <chenjunc84@gmail.com>
2022-09-02 19:00:55 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ff48b8e3-3168-483b-9f0e-223aca5cf701';
+export const MODAL_CLOSE_SEC = 2.5;

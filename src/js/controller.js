d854b3236db85c9e2ed1a669874d7a721e5cbcb7
MosinLover <chenjunc84@gmail.com>
2022-09-02 19:10:16 -0400

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -107,6 +107,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log(`Welcome to the application!`);
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -115,7 +119,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log(`hello world`);
-  // controlServings();
+  newFeature();
 };
 init();
